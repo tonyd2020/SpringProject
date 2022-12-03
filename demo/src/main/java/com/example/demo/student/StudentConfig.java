a3851ef080c74f4a62c92627531bccce2867a058
tonyd2020 <tonyd2ipad@gmail.com>
2022-12-03 18:22:42 +1100

    Initial commit

---
+++ demo/src/main/java/com/example/demo/student/StudentConfig.java
@@ -0,0 +1,32 @@
+package com.example.demo.student;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.time.LocalDate;
+import java.time.Month;
+import java.util.List;
+
+@Configuration
+public class StudentConfig {
+
+//    @Bean
+//    CommandLineRunner commandLineRunner(StudentRepository repository) {
+//        return args -> {
+//            Student tony = new Student(
+//                    "Tony",
+//                    "tonyd2ipad@me.com",
+//                    LocalDate.of(1965, Month.OCTOBER, 31));
+//
+//            Student cherry = new Student(
+//                    "Cherry",
+//                    "cherry@me.com",
+//                    LocalDate.of(1967, Month.OCTOBER, 19));
+//
+//            repository.saveAll(List.of(tony, cherry));
+//        };
+//
+//    }
+
+}
